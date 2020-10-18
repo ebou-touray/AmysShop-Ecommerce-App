96c87e2c84b674dd1ea532e40a206320c53bb830
ebou-touray <60345712+ebou-touray@users.noreply.github.com>
2020-10-18 20:11:26 +0300

    Added a quantity select & cart button and a Cart Component

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,8 +3,12 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
+// Product list action
 export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST });
@@ -25,3 +29,26 @@ export const listProducts = () => async (dispatch) => {
     });
   }
 };
+
+// Product details action
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/products/${id}`);
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
